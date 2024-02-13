7712b6a799399742f60dba4f3f236ebe0ae9c920
adrienmonks11 <133902459+adrienmonks11@users.noreply.github.com>
2024-02-13 00:36:37 -0500

    Delete api_keys.py

--- api_keys.py
+++
@@ -1,9 +0,0 @@
-# football-data api key
-football_api = {'X-Auth-Token': '7b7fd24245ee4bbeafef504d60eb66cc'}
-
-
-# api-football key
-api_football = {
-    'x-rapidapi-host': "api-football-v1.p.rapidapi.com/v3",
-    'x-rapidapi-key': "54b1042a60msh7f1945310248f29p14fd07jsn5a566bc4e4ce"
-}
